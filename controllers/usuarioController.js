a53a62e15d3dbda53776dacb427b6bee342036ac
Rodrigo <istikis@gmail.com>
2021-03-15 14:05:59 +0100

    creación servidor-conexión BD-routing usuarios-prueba endpoint

---
+++ controllers/usuarioController.js
@@ -0,0 +1,17 @@
+const Usuario = require('../models/Usuario');
+
+exports.nuevoUsuario = async (req, res) => {
+
+  // Verificar si el usuario ya estuvo registrado
+  const { email } = req.body;
+
+  let usuario = await Usuario.findOne({ email });
+
+  if (usuario) {
+    return res.status(400).json({ msg: 'El usuario ya esta registrado' });
+  }
+
+  usuario = await new Usuario(req.body);
+  usuario.save();
+  res.json({ msg: 'Usuario Creado con Éxito' });
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+// Crear el Servidor
+const app = express();
+
+// Conectar la DB
+conectarDB();
+
+// Puerto de la app
+const port = process.env.PORT || 4000;
+
+// Habilitar leer los valores de un body
+app.use(express.json());
+
+// Rutas de la App
+app.use('/api/usuarios', require('./routes/usuarios'));
+
+// Arrancar al App
+app.listen(port, '0.0.0.0', () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
+})
